b8eae8cbafc8be5ec6e96b2e31ab8b022b3574b0
Shang Zhou Xia <shangxzx@gmail.com>
2017-10-22 03:01:18 -0400

    Updates to personal project using alias

---
+++ Final/sendfile/server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    app.use(express.static((__dirname, '/../Final')));
+});
+
+app.listen(8080);
